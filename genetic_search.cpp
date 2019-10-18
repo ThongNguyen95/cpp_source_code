9fe72bfba8aaeca6d20b4f2cda8a221a8813d5ff
Thong Nguyen <ducthong1995@berkeley.edu>
2019-10-17 23:04:52 -0700

    genetic search update

--- genetic_search.cpp
+++ genetic_search.cpp
@@ -1,12 +1,26 @@
 #include <iostream>
 #include <array>
+#include <vector>
 #include <time.h>
 #include <algorithm>
+#include <string>
 using namespace std;
 
-//Function Prototypes
-int getSeed(int);
+#define POPULATION_SIZE 100
+#define GENERATIONS 100
 
+//Function Prototypes
+class Path;
+class CostTable;
+int getSeed(int);
+Path* reproduce(const Path*, const Path*);
+void mutate(Path*);
+Path* genetic_search(vector<Path*>&, const CostTable*);
+void cleanup(vector<Path*>);
+bool sortMethod(Path* a, Path* b);
+bool find(array<int, 8>, int first, int size, int val);
+
+//This is used as a reference to calculate the path cost
 class CostTable {
 	int cities[9][9];
 
@@ -45,33 +59,55 @@ public:
 	int getCost(int cityA, int cityB) const {
 		return cities[cityA][cityB];
 	}
-
 };
 
-
-
+// This is the data structure that hold each individual in the population
 class Path {
 	array<int, 8> sol = { 1,2,3,4,5,6,7,8 };
-	int pathCost;		//Fitness
+	int pathCost = 0;		//Fitness
 public:
 	Path() {}
 
+	//Copy Constructor
+	Path(Path* other) {
+		array<int, 8> temp = other->getSol();
+		for (unsigned int i = 0; i < temp.size(); i++) {
+			sol[i] = temp[i];
+		}
+		pathCost = other->getPathCost();
+	}
 	Path(bool randomized) {
 		if (randomized)
 			this->randomize();
 	}
 
 	//Constructor to create child from parents
-	Path(const Path& ind1, const Path& ind2, unsigned int splPoint) {
-		if (splPoint < 8) {
-			for (int i = 0; i <= splPoint; i++)
-				sol[i] = ind1.getElement(i);
-			for (int i = splPoint + 1; i < 8; i++)
-				sol[i] = ind2.getElement(i);
+	Path(const Path* ind1, const Path* ind2, unsigned int splPoint) {
+		for (unsigned int i = 0; i < 8; i++) {
+			if (i <= splPoint) {
+				sol[i] = ind1->getElement(i);
+			}
+			else {
+				int temp = ind2->getElement(i);
+				bool found = find(sol, 0, i, temp);
+				if (!found) {
+					sol[i] = temp;
+				}
+				else {
+					for (int k = 0; k < 8; k++) {
+						temp = ind2->getElement(k);
+						found = find(sol, 0, i, temp);
+						if (!found) {
+							sol[i] = temp;
+							break;
+						}
+					}
+				}
+			}
 		}
-		else
-			Path();
 	}
+
+	array<int, 8> getSol() { return sol; }
 
 	int getElement(unsigned int index) const {
 		if (index < sol.size()) return sol[index];
@@ -83,37 +119,114 @@ public:
 		random_shuffle(sol.begin(), sol.end(),getSeed);
 	}
 
-	void setPathCost(const CostTable& costTable) {
+	void setPathCost(const CostTable* costTable) {
 		pathCost = 0;
-		for (int i = 0; i < sol.size() - 1; i++) {
-			int cost = costTable.getCost(sol[i], sol[i + 1]);
+		for (unsigned int i = 0; i < sol.size() - 1; i++) {
+			int cost = costTable->getCost(sol[i], sol[i + 1]);
 			pathCost = pathCost + cost;
 		}
 	}
 
-	int getPathCost() { return pathCost; }
+	int getPathCost() const { return pathCost; }
 
 	void displayPath() {
 		cout << "Order: ";
-		for (int i = 0; i < sol.size(); i++) {
+		for (unsigned int i = 0; i < sol.size(); i++) {
 			cout << sol[i];
 			if (i < sol.size() - 1) cout << "->";
 		}
 		cout << endl;
 	}
+
+	void swapElements(unsigned int i, unsigned int j) {
+		if (i != j && i < 8 && j < 8) {
+			int temp = sol[i];
+			sol[i] = sol[j];
+			sol[j] = temp;
+		}
+	}
 };
+
 
 int main() {
 	srand(time(NULL)); // for randomization
 	CostTable* costTable = new CostTable();
-	Path path1(true);
-	path1.displayPath();
-	Path path2(true);
-	path2.displayPath();
-	Path path3(path1, path2, 4);
-	path3.displayPath();
+	vector<Path*> population;
+	for (int i = 0; i < POPULATION_SIZE; i++) {
+		Path* temp = new Path(true);	//generate random paths
+		temp->setPathCost(costTable);
+		population.push_back(temp);
+	}
+	Path* result = genetic_search(population, costTable);
+
+	cout << "\nResult: " << endl;
+	result->displayPath();
+	cout << "Cost: " << result->getPathCost() << endl;
+
 	delete costTable;
+	cleanup(population);
 	return 0;
 }
 
-int getSeed(int i) { return rand()%i; }
+// The actual genetic algorithm
+Path* genetic_search(vector<Path*>& population, const CostTable* costTable) {
+	vector<Path*> new_population;
+	int pop_size = population.size();
+	int bestCost = population.front()->getPathCost();
+	int i = 0;
+	while (bestCost > 20 && i < GENERATIONS) {	// Run the algorithm until path cost found is no greater than 20
+												// or until some certain time (100 GENERATIONS in this case)
+		for (int j = 0; j < pop_size; j++) {		//Produce new offsprings
+			Path* parent1 = population[j];			//Selection for reproducing
+			int temp_index = (j < pop_size - 1) ? (j + 1) : (pop_size - 1);
+			Path* parent2 = population[temp_index];	//Selection for reproducing
+			Path* child = reproduce(parent1, parent2);	//Crossover
+			mutate(child);								//Mutation
+			child->setPathCost(costTable);
+			new_population.push_back(child);
+		}
+		
+		cleanup(population);	//Cleanup allocated memory
+		population = new_population;
+		new_population.clear();
+
+		sort(population.begin(), population.end(), sortMethod);	//sort based on fitness test(path-cost)
+		cout << "Generation #" << i + 1 << ". Best path cost: "
+			<< population.front()->getPathCost() << endl;
+		bestCost = population.front()->getPathCost();
+		i++;
+	}
+	return population.front();
+}
+
+Path* reproduce(const Path* parent1, const Path* parent2) {
+	int index = rand() % 8;
+	return new Path(parent1, parent2, index);
+}
+
+void mutate(Path* child) {
+	int index1 = rand() % 8;
+	int index2;
+	do { index2 = rand() % 8; } while (index1 == index2);
+	child->swapElements(index1, index2);	//Swap element at 2 random indexes
+}
+
+void cleanup(vector<Path*> population) {
+	while (!population.empty()) {
+		Path* temp = population.back();
+		population.pop_back();
+		delete temp;
+	}
+}
+bool sortMethod(Path* a, Path* b) {		//Use with the built-in sort algorithm to sort the population
+	if (a->getPathCost() < b->getPathCost()) return true;
+	else return false;
+}
+int getSeed(int i) { return rand()%i; }	//Seed for generating random value
+
+bool find(array<int, 8> arr, int first, int size, int val) { //Check if a value is in an array
+	for (int i = first; i < size; i++) {
+		if (arr[i] == val) return true;
+	}
+	return false;
+}
